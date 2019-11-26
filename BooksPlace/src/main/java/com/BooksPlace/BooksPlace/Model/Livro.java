d93d707640b3e7c5007e41c5eca1fd246b51e2cf
flavianny <flvdiniz91@gmail.com>
2019-11-26 11:22:18 -0300

    projeto BooksplaceV1.2

--- BooksPlace/src/main/java/com/BooksPlace/BooksPlace/Model/Livro.java
+++ BooksPlace/src/main/java/com/BooksPlace/BooksPlace/Model/Livro.java
@@ -30,6 +30,10 @@ public class Livro {
 	@Size(max = 60, message = "A Descrição não deve obter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotEmpty(message = "O campo Gerero é obrigatório")
+	@Size(max = 60, message = "O Genero não deve obter mais de 60 caracteres")
+	private String genero;
+	
 	/**
 	 * @deprecated Essa variável não deveria ser utilizada com o tipo String,
 	 * e sim com o tipo int.
@@ -42,52 +46,77 @@ public class Livro {
 	@Size(max = 40, message = "O Autor não deve obter mais de 40 caracteres")
 	private String autor;
 	
+	@NotEmpty(message = "O campo ISBN é obrigatório")
+	@Size(max = 40, message = "O ISBN não deve obter mais de 40 caracteres")
+	private String isbn;
+	
 	@Enumerated(EnumType.STRING)
 	private StatusLivro status;
-	 
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
 	public String getAno() {
 		return ano;
 	}
+
 	public void setAno(String ano) {
 		this.ano = ano;
 	}
-	
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
 	public StatusLivro getStatus() {
 		return status;
 	}
+
 	public void setStatus(StatusLivro status) {
 		this.status = status;
 	}
-	
-	
-	public boolean isIndisponivel() {
-		return StatusLivro.INDISPONIVEL.equals(this.status); 
-	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,11 +124,14 @@ public class Livro {
 		result = prime * result + ((ano == null) ? 0 : ano.hashCode());
 		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
 		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((genero == null) ? 0 : genero.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
 		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -124,7 +156,17 @@ public class Livro {
 				return false;
 		} else if (!descricao.equals(other.descricao))
 			return false;
+		if (genero == null) {
+			if (other.genero != null)
+				return false;
+		} else if (!genero.equals(other.genero))
+			return false;
 		if (id != other.id)
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
 			return false;
 		if (status != other.status)
 			return false;
@@ -135,5 +177,6 @@ public class Livro {
 			return false;
 		return true;
 	}
-	
+	 
+
 }
