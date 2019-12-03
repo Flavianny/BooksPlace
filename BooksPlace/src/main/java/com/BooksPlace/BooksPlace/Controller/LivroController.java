f23b2907f985b8b91b7f8a3a6a854eb708eb7027
airla <airlavcds@gmail.com>
2019-12-03 09:45:37 -0300

    alteracões feita no emprestimo

--- BooksPlace/src/main/java/com/BooksPlace/BooksPlace/Controller/LivroController.java
+++ BooksPlace/src/main/java/com/BooksPlace/BooksPlace/Controller/LivroController.java
@@ -22,20 +22,22 @@ import com.BooksPlace.BooksPlace.Model.Livro;
 import com.BooksPlace.BooksPlace.Model.StatusLivro;
 import com.BooksPlace.BooksPlace.repository.Livros;
 import com.BooksPlace.BooksPlace.repository.filter.LivroFilter;
+
 /**
  * Classe utilizada para controlar as requisições feitas para entidade livro.
+ * 
  * @author flavianny
  *
  */
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
-	
+
 	@Autowired
 	private Livros livros;
 
 	private Livro livro;
-	
+
 	private StreamedContent imagem;
 
 	public StreamedContent getImagem() {
@@ -45,58 +47,58 @@ public class LivroController {
 	public void setImagem(StreamedContent imagem) {
 		this.imagem = imagem;
 	}
-	
-	  public StreamedContent getImagemAtual() throws IOException { FacesContext
-		  context = FacesContext.getCurrentInstance(); if (context.getCurrentPhaseId()
-		  == PhaseId.RENDER_RESPONSE) { return new DefaultStreamedContent(); } else {
-		  return new DefaultStreamedContent(new
-		  ByteArrayInputStream(livro.getCapa())); 
-		  } 
-	  }
-	
+
+	public StreamedContent getImagemAtual() throws IOException {
+		FacesContext context = FacesContext.getCurrentInstance();
+		if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+			return new DefaultStreamedContent();
+		} else {
+			return new DefaultStreamedContent(new ByteArrayInputStream(livro.getCapa()));
+		}
+	}
+
 	/**
-	 * Método novo
-	 * Responsável por atender a requisição ("/novo") a qual 
-	 * exibirá uma view com o formulario de cadastro de livro.
+	 * Método novo Responsável por atender a requisição ("/novo") a qual exibirá uma
+	 * view com o formulario de cadastro de livro.
+	 * 
 	 * @author flavianny
 	 */
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroLivro");
 		mv.addObject(new Livro());
-		return mv; 
+		return mv;
 	}
-	
+
 	/**
-	 * Método buscarLivro
-	 * Responsável por realizar a busca de todos os livros na 
+	 * Método buscarLivro Responsável por realizar a busca de todos os livros na
 	 * base de dados.
+	 * 
 	 * @return ModelAndView retorna o objeto com a listagem.
 	 * @author mycaell
 	 */
 	@RequestMapping
 	public ModelAndView buscarLivro(@ModelAttribute("filtro") LivroFilter filtro) {
-		
-		
+
 //		List<Livro> todosLivros = filtro.getTextoFiltro() == null ? livros.findAll() : livros.findByTituloContainingOrAutoresContainingOrGeneroContaining(filtro.getTextoFiltro(), filtro.getListaFiltro(), filtro.getTextoFiltro());
-		List<Livro> todosLivros = filtro.getTextoFiltro() == null ? livros.findAll() : livros.findByTituloContainingOrGeneroContaining(filtro.getTextoFiltro(),filtro.getTextoFiltro());
-		
+		List<Livro> todosLivros = filtro.getTextoFiltro() == null ? livros.findAll()
+				: livros.findByTituloContainingOrGeneroContaining(filtro.getTextoFiltro(), filtro.getTextoFiltro());
+
 		ModelAndView mv = new ModelAndView("PesquisaDeLivros");
 		mv.addObject("livros", todosLivros);
-		return mv; 
-	} 
-	
-	
+		return mv;
+	}
+
 	/**
-	 * Método salvar
-	 * Utilizado para salvar um livro no banco de dados.
-	 * @param livro livro que contém os dados a serem salvos.
+	 * Método salvar Utilizado para salvar um livro no banco de dados.
+	 * 
+	 * @param livro  livro que contém os dados a serem salvos.
 	 * @param errors representa os possíveis erros de validação do livro.
 	 * @author flavianny
 	 */
 	@RequestMapping(method = RequestMethod.POST)
 
-	public ModelAndView salvar(@RequestParam("file") MultipartFile file, @Validated Livro livro, Errors erros) {	
+	public ModelAndView salvar(@RequestParam("file") MultipartFile file, @Validated Livro livro, Errors erros) {
 		ModelAndView mv = new ModelAndView("CadastroLivro");
 		if (erros.hasErrors()) {
 			return mv;
@@ -110,15 +112,16 @@ public class LivroController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//uploadImagemLivro(file);
+		// uploadImagemLivro(file);
 		livros.save(livro);
 		mv.addObject("mensagem", "Livro salvo com sucesso");
-		return mv;	
+		return mv;
 	}
-	
+
 	/**
-	 * Método todosStatusLivro
-	 * Utilizado para retornar os status do livro definidos pelo Enum StatusLivro.
+	 * Método todosStatusLivro Utilizado para retornar os status do livro definidos
+	 * pelo Enum StatusLivro.
+	 * 
 	 * @return object status do livro disponíveis.
 	 * @author flavianny
 	 */
